e45858be7442f7ffc2aea97197cb1c6848da6a7e
andrefdev <andreforsythx@gmail.com>
2025-05-23 20:48:07 -0500

    add: setnewpassword

---
+++ src/auth/dto/set-new-password.dto.ts
@@ -0,0 +1,1 @@
+import { ApiProperty } from '@nestjs/swagger';import { IsNotEmpty, IsString, MinLength } from 'class-validator';export class SetNewPasswordDto {  @IsNotEmpty()  @IsString()  @MinLength(6)  @ApiProperty({ minLength: 6 })  newPassword: string;  @IsNotEmpty()  @IsString()  @ApiProperty()  path: string;}
